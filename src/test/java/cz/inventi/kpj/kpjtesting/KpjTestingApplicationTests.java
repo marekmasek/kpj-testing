78426c22e9aee5fcbcb27c480072416aa3e9e7e2
Marek Mašek <24.mara@seznam.cz>
2022-05-02 20:06:15 +0200

    fixed

--- src/test/java/cz/inventi/kpj/kpjtesting/KpjTestingApplicationTests.java
+++ src/test/java/cz/inventi/kpj/kpjtesting/KpjTestingApplicationTests.java
@@ -1,9 +1,16 @@
 package cz.inventi.kpj.kpjtesting;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
- *
  * 1. Create Spring Boot Test (Integration test with the application context
  * loading) <br/>
  * 2. Provide mock instance of {@link HelloWorldService} <br/>
@@ -14,37 +21,41 @@ import org.springframework.beans.factory.annotation.Autowired;
  * 3.1. Verify the {@link HelloWorldService#helloWorld()} was called<br/>
  * 3.2. Assert the {@link KpjTesting#printEcho(String)} returns "KPJ echo"<br/>
  */
-
+@SpringBootTest
 class KpjTestingApplicationTests {
 
+    @MockBean
+    HelloWorldService helloWorldService;
 
-  HelloWorldService helloWorldService;
+    @Autowired
+    KpjTesting kpjTesting;
 
-  @Autowired
-  KpjTesting kpjTesting;
+    @Test
+    void testHelloWorld() {
+        // given
+        // Setum mock
+        when(helloWorldService.helloWorld()).thenReturn("Hello World");
 
-  void testHelloWorld() {
-    // given
-    // Setup mock
+        // when
+        kpjTesting.printHelloWorld();
 
-    // when
-    kpjTesting.printHelloWorld();
+        // then
+        // Verify the HelloWorldService#helloWorld() was called
+        verify(helloWorldService).helloWorld();
+    }
 
-    // then
-    // Verify the HelloWorldService#helloWorld() was called
+    @Test
+    void testPrintEcho() {
+        // given
+        // setup mock
+        when(helloWorldService.echo(any())).thenReturn("KPJ echo");
 
-  }
+        // when
+        String result = kpjTesting.printEcho("KPJ echo");
 
-  void testPrintEcho() {
-    // given
-    // setup mock
-
-    // when
-    String result = kpjTesting.printEcho("KPJ echo");
-
-    // then
-    // Add assert the result is equals to "KPJ echo"
-
-  }
+        // then
+        // Add assert the result is equals to "KPJ echo"
+        assertEquals("KPJ echo", result);
+    }
 
 }
--- src/test/java/cz/inventi/kpj/kpjtesting/KpjTestingTest.java
+++ src/test/java/cz/inventi/kpj/kpjtesting/KpjTestingTest.java
@@ -1,7 +1,16 @@
 package cz.inventi.kpj.kpjtesting;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * 1. Create Spy on the {@link KpjTesting#helloWorldService} field <br/>
@@ -13,52 +22,55 @@ import org.mockito.quality.Strictness;
 @MockitoSettings(strictness = Strictness.LENIENT)
 public class KpjTestingTest {
 
+    @Spy
+    HelloWorldService helloWorldService = new HelloWorldServiceImpl();
 
-  HelloWorldService helloWorldService = new HelloWorldServiceImpl();
+    @InjectMocks
+    KpjTesting kpjTesting;
 
-
-  KpjTesting kpjTesting;
-
-  void setUp() {
-    // setup mocks for all tests
-    // hint you have to use doReturn for Spy
-    // Change behavior of HelloWorldServiceImpl#echo(String)
-    // to always returns "KPJ Rulez"
-
-  }
-
-  void testPrintHelloWorld() {
-    // no given
-    // when
-    kpjTesting.printHelloWorld();
-
-    // then
-    // verify the HelloWorldServiceImpl#helloWorld() is called
-
-  }
-
-  void testPrintEcho() {
-    // no given
-    // when
-    String result = kpjTesting.printEcho("Print ECHO");
-
-    // then
-    // add assert the result equals "KPJ Rulez"
-
-  }
-
-  static class HelloWorldServiceImpl implements HelloWorldService {
-
-    @Override
-    public String helloWorld() {
-      return "Hello world";
+    @BeforeEach
+    void setUp() {
+        // setup mocks for all tests
+        // hint you have to use doReturn for Spy
+        // Change behavior of HelloWorldServiceImpl#echo(String)
+        // to always returns "KPJ Rulez"
+        when(helloWorldService.echo(anyString())).thenReturn("KPJ Rulez");
     }
 
-    @Override
-    public String echo(String echo) {
-      return echo;
+    @Test
+    void testPrintHelloWorld() {
+        // no given
+        // when
+        kpjTesting.printHelloWorld();
+
+        // then
+        // verify the HelloWorldServiceImpl#helloWorld() is called
+        verify(helloWorldService).helloWorld();
     }
 
-  }
+    @Test
+    void testPrintEcho() {
+        // no given
+        // when
+        String result = kpjTesting.printEcho("Print ECHO");
+
+        // then
+        // add assert the result equals "KPJ Rulez"
+        assertEquals("KPJ Rulez", result);
+    }
+
+    static class HelloWorldServiceImpl implements HelloWorldService {
+
+        @Override
+        public String helloWorld() {
+            return "Hello world";
+        }
+
+        @Override
+        public String echo(String echo) {
+            return echo;
+        }
+
+    }
 
 }
